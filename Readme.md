863879d31918a61f8844157b0121c1bc96e136ca
Dan Hammer <daniel.m.hammer@gmail.com>
2020-08-10 21:48:07 -0500

    Added option to expect a specific user agent

--- Readme.md
+++ Readme.md
@@ -1,34 +1,92 @@
 Simple User Agent grabber written in Go.
 
 # Usage
-## Create a User Agent grabbing session:  
+
+## Create a User Agent grabbing session:
+
 GET /create
 
 Response :
+
 ```json
 {
-"id" : "RandomIDString",
-"agent": ""
+  "id": "RandomIDString",
+  "agent": ""
 }
 ```
+
 ## Save User Agent
+
 GET /agent/{id}
 
 Response :
+
 ```json
 {
-"id" : "id",
-"agent": "AgentString"
+  "id": "id",
+  "agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:79.0) Gecko/20100101 Firefox/79.0"
 }
 ```
 
 ## Get Saved User Agent
+
 GET /verify/{id}
 
 Response :
+
 ```json
 {
-"id" : "id",
-"agent": "AgentString"
+  "id": "id",
+  "agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:79.0) Gecko/20100101 Firefox/79.0"
+}
+```
+
+## Create Expected User Agent
+
+POST /expect/create
+
+Body :
+
+```json
+{
+  "expected_os": "Intel Mac OS X 10.15",
+  "expected_browser": "Firefox",
+  "expected_version": "79.0"
+}
+```
+
+Response :
+
+```json
+{
+  "id": "id",
+  "agent": "",
+  "os": "",
+  "browser": "",
+  "version": "",
+  "expected_os": "Intel Mac OS X 10.15",
+  "expected_browser": "Firefox",
+  "expected_version": "79.0",
+  "result": false
+}
+```
+
+## Verify Expected User Agent
+
+GET /expect/verify/{id}
+
+Response :
+
+```json
+{
+  "id": "id",
+  "agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:79.0) Gecko/20100101 Firefox/79.0",
+  "os": "Intel Mac OS X 10.15",
+  "browser": "Firefox",
+  "version": "79.0",
+  "expected_os": "Intel Mac OS X 10.15",
+  "expected_browser": "Firefox",
+  "expected_version": "79.0",
+  "result": true
 }
 ```
--- app/main.go
+++ app/main.go
@@ -10,11 +10,22 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/patrickmn/go-cache"
 	"github.com/teris-io/shortid"
+	"github.com/mssola/user_agent"
 )
 
 type Data struct {
 	ID string `json:"id"`
+
 	Agent string `json:"agent"`
+	OS string `json:"os"`
+	Browser string `json:"browser"`
+	Version string `json:"version"`
+
+	ExpectedOS string `json:"expected_os"`
+	ExpectedBrowser string `json:"expected_browser"`
+	ExpectedVersion string `json:"expected_version"`
+
+	Result bool `json:"result"`
 }
 
 func main() {
@@ -28,17 +39,44 @@ func main() {
 		w.Header().Set("Content-Type", "application/json")
 		id, _ := shortid.Generate()
 		data := Data{ID:id}
-		c.Set(id, &data, cache.DefaultExpiration)
+		c.Set(id, data, cache.DefaultExpiration)
+		json.NewEncoder(w).Encode(data)
+	})
+
+	router.HandleFunc("/expect/create", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "application/json")
+		id, _ := shortid.Generate()
+		var data Data
+		err := json.NewDecoder(r.Body).Decode(&data)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+
+		data.ID = id
+		c.Set(id, data, cache.DefaultExpiration)
 		json.NewEncoder(w).Encode(data)
 	})
 
 	router.HandleFunc("/agent/{id}", func(w http.ResponseWriter, r *http.Request){
 		id := mux.Vars(r)["id"]
 		ua := r.Header.Get("User-Agent")
-		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, "%s:%s", id, ua)
-		data := Data{ID:id, Agent:ua}
-		c.Set(id, &data, cache.DefaultExpiration)
+
+		if x, found := c.Get(id); found {
+			data := x.(Data)
+			data.Agent = ua
+			ua := user_agent.New(data.Agent)
+
+			data.OS = ua.OS()
+			browser, version := ua.Browser()
+			data.Browser = browser
+			data.Version = version
+
+			c.Set(id, data, cache.DefaultExpiration)
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
 	})
 
 	router.HandleFunc("/verify/{id}", func(w http.ResponseWriter, r *http.Request){
@@ -46,7 +84,25 @@ func main() {
 		id := mux.Vars(r)["id"]
 
 		if x, found := c.Get(id); found {
-			data := x.(*Data)
+			data := x.(Data)
+			json.NewEncoder(w).Encode(data)
+		} else {
+			json.NewEncoder(w).Encode(Data{ID: id, Agent: "NA"})
+		}
+	})
+
+	router.HandleFunc("/expect/verify/{id}", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		id := mux.Vars(r)["id"]
+		
+		if x, found := c.Get(id); found {
+			data := x.(Data)
+			
+			if (data.OS == data.ExpectedOS && data.Browser == data.ExpectedBrowser && data.Version == data.ExpectedVersion) {
+				data.Result = true
+			} else {
+				data.Result = false
+			}
 			json.NewEncoder(w).Encode(data)
 		} else {
 			json.NewEncoder(w).Encode(Data{ID: id, Agent: "NA"})

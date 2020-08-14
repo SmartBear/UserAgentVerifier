54fed4944c96652267463ec798abc7b4541fb11b
Dan Hammer <daniel.m.hammer@gmail.com>
2020-08-14 15:17:28 -0500

    Removing my fork of user_agent

--- app/main.go
+++ app/main.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/patrickmn/go-cache"
 	"github.com/teris-io/shortid"
-	"github.com/DanMHammer/user_agent"
+	"github.com/mssola/user_agent"
 )
 
 type Data struct {
